271a4fcdab4bf61f47bc4c820b9baa30f2dcb0d3
Asta <astaviz@gmail.com>
2020-11-25 15:16:30 +0100

    second day

---
+++ day_3/day_3_ex23.py
@@ -0,0 +1,22 @@
+# variable_list = [1, 2, 3, 4, 5]
+# new_list = [variable_list[0], variable_list[0]**0, variable_list[0]**1, variable_list[0]**2,variable_list[0]**3]
+# new_list_2 = [variable_list[1], variable_list[1]**0, variable_list[1]**1, variable_list[1]**2,variable_list[1]**3]
+# new_list_3 = [variable_list[2], variable_list[2]**0, variable_list[2]**1, variable_list[2]**2,variable_list[2]**3]
+# new_list_4 = [variable_list[3], variable_list[3]**0, variable_list[3]**1, variable_list[3]**2,variable_list[3]**3]
+# new_list_5 = [variable_list[4], variable_list[4]**0, variable_list[4]**1, variable_list[4]**2,variable_list[4]**3]
+# print(new_list)
+# print(new_list_2)
+# print(new_list_3)
+# print(new_list_4)
+# print(new_list_5)
+
+a=1
+b=2
+c=3
+d=4
+e=5
+print(a, a**0, a**1, a**2, a**3)
+print(b, b**0, b**1, b**2, b**3)
+print(c, c**0, c**1, c**2, c**3)
+print(d, d**0, d**1, d**2, d**3)
+print(e, e**0, e**1, e**2, e**3)
