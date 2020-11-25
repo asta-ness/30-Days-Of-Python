60485539335b6fcbf1ca4b359e6207c24e8f444d
Asta <astaviz@gmail.com>
2020-11-25 13:38:44 +0100

    second day

---
+++ day_3/day_3_ex11.py
@@ -0,0 +1,3 @@
+variable_x = input('Enter x: ')
+print('Variable y is: ',int(variable_x)**2 + 6*int(variable_x) + 9)
+
---
+++ day_3/day_3_ex_8.py
@@ -0,0 +1,3 @@
+variable_x = input('Enter x: ')
+variable_y = 2*int(variable_x) - 2
+print(variable_y)
