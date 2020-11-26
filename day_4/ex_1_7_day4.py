2bb3db81d1d10a8c25bc316443e888eadf8d65c7
Asta <astaviz@gmail.com>
2020-11-26 16:59:18 +0100

    day 3 with Python

---
+++ day_4/ex_1_7_day4.py
@@ -0,0 +1,26 @@
+#ex1 Concatenate the string 'Thirty', 'Days', 'Of', 'Python' to a single string, 'Thirty Days Of Python'.
+word_1 = 'Thirty'
+word_2 = 'Days'
+word_3 = 'Of'
+word_4 = 'Python'
+space = '  '
+sentence = word_1 + space+word_2+space+word_3+space+word_4
+print(sentence)
+
+#ex2 Concatenate the string 'Coding', 'For' , 'All' to a single string, 'Coding For All'.
+word_1 = 'Coding'
+word_2 = 'For'
+word_3 = 'All'
+space = '  '
+sentence = word_1 + space+word_2+space+word_3
+print(sentence)
+
+#ex3-4-5-6-7 Declare a variable named company and assign it to an initial value "Coding For All"...
+company_name = 'Coding For All'
+print(company_name)
+print(len(company_name))
+print(company_name.upper())
+print(company_name.lower())
+
+#ex8-
+
